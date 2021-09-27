fa239b8a0ee23b8363a71ff6a15557a035b3f5b3
Yann Touil <yanntouil@gmail.com>
2021-09-27 13:19:36 +0200

    Added first stage of form validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,3 +27,52 @@ function launchModal() {
   (el) => el.addEventListener("click", 
     (e) => modalbg.style.display = ''
 ));
+
+/**
+ * Form data validation
+ */
+function validate() {
+  const form = document.forms["reserve"];
+  let formErrors = 0;
+  // Field firstname must have a minimum 2 characters and can not be empty
+  const firstName = form.querySelectorAll('input[name="first"]')[0];
+  if (firstName.value.length <= 1) {
+    formErrors++;
+  }
+  // Field lastname must have a minimum 2 characters and can not be empty
+  const lastName = form.querySelectorAll('input[name="last"]')[0];
+  if (lastName.value.length <= 1) {
+    formErrors++;
+  }
+  // Field email must be a valid email
+  const email = form.querySelectorAll('input[name="email"]')[0];
+  const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!email.value.match(mailRegex)) {
+    formErrors++;
+  }
+  // Field quantity must be not empty, numeric and integer
+  const quantity = form.querySelectorAll('input[name="quantity"]')[0];
+  if (!(quantity.value != '' && !isNaN(quantity.value) && Number.isInteger(Number(quantity.value)))) {
+    formErrors++;
+  }
+  // One of fields location must be selected
+  const radioList = form.querySelectorAll('input[name="location"]');
+  let radioValue;
+  radioList.forEach((el) => {
+      if (el.checked) radioValue = el.value;
+    }
+  );
+  if (!radioValue) {
+    formErrors++;
+  }
+  // Field terms must be checked
+  const terms = form.querySelectorAll('input[name="terms"]')[0];
+  if (!terms.checked) {
+    formErrors++;
+  }
+  // Check validation errors
+  if (formErrors > 0) {
+    return false;
+  }
+  
+}
