71bfbcf6482f6eaf5ad423d607621e88ba5dab57
Yann Touil <yanntouil@gmail.com>
2021-09-27 13:52:06 +0200

    Added validation messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -37,23 +37,35 @@ function validate() {
   // Field firstname must have a minimum 2 characters and can not be empty
   const firstName = form.querySelectorAll('input[name="first"]')[0];
   if (firstName.value.length <= 1) {
+    showFieldError(firstName);
     formErrors++;
+  } else {
+    hideFieldError(firstName);
   }
   // Field lastname must have a minimum 2 characters and can not be empty
   const lastName = form.querySelectorAll('input[name="last"]')[0];
   if (lastName.value.length <= 1) {
+    showFieldError(lastName);
     formErrors++;
+  } else {
+    hideFieldError(lastName);
   }
   // Field email must be a valid email
   const email = form.querySelectorAll('input[name="email"]')[0];
   const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if (!email.value.match(mailRegex)) {
+    showFieldError(email);
     formErrors++;
+  } else {
+    hideFieldError(email);
   }
   // Field quantity must be not empty, numeric and integer
   const quantity = form.querySelectorAll('input[name="quantity"]')[0];
   if (!(quantity.value != '' && !isNaN(quantity.value) && Number.isInteger(Number(quantity.value)))) {
+    showFieldError(quantity);
     formErrors++;
+  } else {
+    hideFieldError(quantity);
   }
   // One of fields location must be selected
   const radioList = form.querySelectorAll('input[name="location"]');
@@ -63,16 +75,36 @@ function validate() {
     }
   );
   if (!radioValue) {
+    showFieldError(radioList[0]);
     formErrors++;
+  } else {
+    hideFieldError(radioList[0]);
   }
   // Field terms must be checked
   const terms = form.querySelectorAll('input[name="terms"]')[0];
   if (!terms.checked) {
+    showFieldError(terms);
     formErrors++;
+  } else {
+    hideFieldError(terms);
   }
   // Check validation errors
   if (formErrors > 0) {
     return false;
   }
-  
+  return false;
+}
+/**
+ * Show field error
+ * @param {HTMLElement} el
+ */
+function showFieldError(el) {
+  el.closest('.formData').dataset.errorVisible = 'true';
+}
+/**
+ * Hide field error
+ * @param el
+ */
+ function hideFieldError(el) {
+  el.closest('.formData').dataset.errorVisible = null;
 }
